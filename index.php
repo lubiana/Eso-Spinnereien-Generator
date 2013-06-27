93b30d2e5ba6574580bddce8c1d643d7a1b742f3
lubiana <lubiana123@gmail.com>
2013-06-27 21:00:35 +0200

    hurr durr

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+  // Einleitung
+  $hurr[] = array(
+    'Unsere Idee ist, ',
+    'Wir planen, ',
+    'Der Erzengel Metatron hat uns auf die Idee gebracht, ',
+    'Geleitet von jahrtausendealtem Erfahrungswissen kamen wir darauf, ',
+    'Der Geist von Wilhelm Reich hat uns aufgetragen, ',
+    'Weil wir die moderne Schulmedizin ablehnen, sehen wir es als unsere Pflicht, ',
+    'Nach jahrelanger Meditation haben uns die Lichtwesen mit der Idee kontaktiert, ',
+    'Weil wir dem Chemtrail-Terror entgegentreten wollen, planen wir, ',
+    'Aufgrund unserer tiefen spirituellen Liebe zu unseren Mitmenschen kamen wir auf den Gedanken, ',
+    'Unser Templerbruder vom alten Orden hat uns geraten, ',
+    'Unsere Investoren vom Planeten Nibiru zwingen uns, ',
+    'Weil wir uns gegen die Infrarot-Nazis zur Wehr setzen möchten, können wir nicht anders, als ',
+    'Von den Erfolgen der Lichtheiler ausgehend ist es uns möglich, ',
+    'Unsere Energiegruppe hat beschlossen, ',
+    'Der Terror der Merkel-Junta lässt uns keine andere Wahl, als ',
+    'Viktor Schaubergers Schriften haben uns dazu inspiriert, ',
+    'Weil wir uns dazu berufen fühlen, in die Fußstapfen Nikola Teslas zu treten, haben wir vor, '
+  );
+
+  // Warum haben sie Superkräfte?
+  $hurr[] = array(
+    'hoch energetisierte ',
+    'tachyonisierte ',
+    'mit Plutonium abgefüllte ',
+    'homöopathisch potenzierte ',
+    'mit Bachblütenessenzen angereicherte ',
+    'mit Orgonenergie bestrahlte ',
+    'nach der Silva-Methode behandelte ',
+    'nach Plänen von Dr. Axel Stoll konstruierte ',
+    'auf vedischer Wissenschaft basierende ',
+    'mit Hilfe der Alpha-Synapsen-Programmierung korrigierte ',
+    'biologisch-dynamisch erzeugte ',
+    'radiästhetisch vibrierende ',
+    'raum-zeitlich nach innen gerichtete '
+  );
+
+  // Und was?
+  $hurr[] = array(
+    'Kopfhörer ',
+    'Müsliriegel ',
+    'Dildos ',
+    'Halbedelsteine ',
+    'Globuli ',
+    'Energiesparlampen ',
+    'Energiekarten ',
+    'Erfrischungsgetränke ',
+    'Bürolampen ',
+    'Akupunkturnadeln ',
+    'Voodoopuppen ',
+    'Abfalleimer ',
+    'Toilettenpapierrollen ',
+    'USB-Kabel ',
+    'Kaffeemaschinen ',
+    'Nickel-Cadmium-Batterien ',
+    'Tennissocken ',
+    'Unterhemden ',
+    'Autoradios ',
+    'Aschenbecher ',
+    'Topflappen ',
+    'Rheumadecken ',
+    'Küchenmesser ',
+    'Parkscheiben ',
+    'Zimmerpflanzen ',
+    'Damenfahrräder ',
+    'Zahnimplantate ',
+    'Bilderrahmen ',
+    'Kopfkissen ',
+    'Käsebrötchen ',
+    'Klaviere '
+  );
+
+  // Was tun wir?
+  $hurr[] = array(
+    'mit kosmischen Strahlungen zu bombardieren ',
+    'mit Mikrowellen und/oder Skalarwellen aufzuladen ',
+    'durch weiße Magie zum schwingen zu bringen ',
+    'in superionisiertes Wasser einzulegen ',
+    'durch alchemistische Rituale zu transformieren ',
+    'mit Urfrequenzen zu beschallen ',
+    'gemäß eines Implosionsstrudels anzuzapfen ',
+    'mit heiligem Radium 88 zu versetzen ',
+    'mit informiertem Wasser zu beträufeln ',
+    'von Indigo-Kindern bemalen zu lassen ',
+    'pentatonisch zu beschallen ',
+    'aurafotografisch zu erfassen ',
+    'pulsierenden Magnetfeldern auszusetzen ',
+    'mit Energieakkumulatoren zu modulieren ',
+    'harmonisch auszupendeln ',
+    'neurolinguistisch zu programmieren '
+  );
+
+  // Und warum?
+  $hurr[] = array(
+    'um den Benzinverbrauch von Kraftfahrzeugen zu reduzieren.',
+    'um das Dirk-Hamer-Syndrom sogenannter Krebspatienten zu behandeln.',
+    'um die Mind Control-Versuche der Illuminaten abzuwehren.',
+    'um unseren Kunden zu mehr Energie und Lebensfreude zu verhelfen.',
+    'um den Kontakt zu verstorbenen Angehörigen zu ermöglichen.',
+    'um der Raffgier der Pharmaindustrie eine Alternative entgegen zu setzen.',
+    'um den Menschen den Kontakt zu ihren früheren Inkarnationen zu erlauben.',
+    'um Leidenden Teufel und Dämonen auszutreiben.',
+    'um Impfungen gegen Tropenkrankheiten unnötig zu machen.',
+    'um Big Pharma einen Strich durch die Rechnung zu machen.',
+    'um die Bevölkerung zu einem gesünderen Leben zu führen.',
+    'um den semitisch-reptiloiden Besatzern entgegen zu treten.',
+    'um auch Laien Astralreisen zu ermöglichen.',
+    'um den ungünstigen Tachyonenfluss in Altbauwohnungen zu korrigieren.',
+    'um die Aura des Anwenders positiv zu beeinflussen.',
+    'um die Selbstreinigungs- und Entschlackungsfähigkeit des Körpers anzuregen.',
+    'um den Bilderbergern auf die Schliche zu kommen..'
+  );
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Eso-Spinnereien-Generator nach Dr. tka</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+.unfug {
+font-size:large;
+font-family:sans-serif;
+margin-left:auto;
+margin-right:auto;
+width:60%;
+} 
+</style>
+</head>
+<body>
+<p class="unfug">
+<?php
+  foreach($hurr as $durr)
+    echo $durr[rand(0, count($durr) -1)];
+?>
+</p>
+</body>
